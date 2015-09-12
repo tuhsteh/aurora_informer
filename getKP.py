4c2772c50c71a46a3d65926510c841b212d6c885
john.sosoka <john.sosoka@gmail.com>
2015-09-11 22:15:10 -0600

    nothing special

--- getKP.py
+++ getKP.py
@@ -5,21 +5,32 @@ import re
 page = requests.get('http://www.spaceweatherlive.com/en/auroral-activity/kp')
 tree = html.fromstring(page.text)
 
-predicted_kp_max = tree.xpath('//td[text()="Predicted Kp max"]/following-sibling::td[1]/text()')
-high_lat_chance = tree.xpath('//td[text()="High latitude"]/following-sibling::td[1]/span/text()')
-middle_lat_chance = tree.xpath('//td[text()="Middle latitude"]/following-sibling::td[1]/span/text()')
-low_lat_chance = tree.xpath('//td[text()="Low latitude"]/following-sibling::td[1]/span/text()')
+predicted_kp_max = ""
+high_lat_chance = ""
+middle_lat_chance = ""
+low_lat_chance = ""
 
-print "%24s:\t%s" % ("High Latitude Chance", high_lat_chance[0])
-print "%24s:\t%s" % ("Mid Latitude Chance", middle_lat_chance[0])
-print "%24s:\t%s" % ("Low Latitude Chance", low_lat_chance[0])
-print "%24s:\t%s" % ("Predicted KP Max", predicted_kp_max[0])
+latitude_options = ['high', 'middle', 'low', 'kp_max']
 
-#Function to clean up results#
+
+def get_aurora_chance(by_lat):
+    if by_lat == "kp_max":
+        predicted_kp_max = tree.xpath('//td[text()="Predicted Kp max"]/following-sibling::td[1]/text()')
+        return predicted_kp_max[0]
+    #high_lat_chance = tree.xpath('//td[text()="High latitude"]/following-sibling::td[1]/span/text()')
+    #middle_lat_chance = tree.xpath('//td[text()="Middle latitude"]/following-sibling::td[1]/span/text()')
+    #low_lat_chance = tree.xpath('//td[text()="Low latitude"]/following-sibling::td[1]/span/text()')
+    #print "%24s:\t%s" % ("High Latitude Chance", high_lat_chance[0])
+    #print "%24s:\t%s" % ("Mid Latitude Chance", middle_lat_chance[0])
+    #print "%24s:\t%s" % ("Low Latitude Chance", low_lat_chance[0])
+    #print "%24s:\t%s" % ("Predicted KP Max", predicted_kp_max[0])
 
 def rip_percent(lat):
     lat_no_percent = lat.replace("%", "")
     print lat_no_percent
 
-#rip_percent(middle_lat_chance[0])
+blah = get_aurora_chance("kp_max")
+print blah
+
+
 
