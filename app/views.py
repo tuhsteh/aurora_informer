416b1b3c268e651f4dd1f1d415a1f92884946cef
john.sosoka <john.sosoka@gmail.com>
2015-09-15 21:43:23 -0600

    templates half-working

--- app/views.py
+++ app/views.py
@@ -1,13 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import iSubscribe
+
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    #if iSubscribe.validate_on_submit():
+    #    print "someone clicked some shit"
+    form = iSubscribe()
+    if form.validate_on_submit:
+        print  "somebody said"# % user_email
     return render_template('index.html',
-                           title='Aurora Informer Home')
+                           title='Aurora Informer', # <-- you can see how title gets populated in the ~/app/templates/base.html
+                           form=form) 
 
 @app.route('/unsubscribe')
 def unsubscribe():
     return render_template('unsubscribe.html',
-                           title='Unsubscribe Aurora Informer')    
+                           title='Unsubscribe from Aurora Informer')    
